4534f0794eb9169552990ba16e109bf8814921bd
DimaSurdiy <dsurdiy@ukr.net>
2022-06-23 18:36:36 +0300

    Timer code refactoring

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,16 +3,17 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const inputEl = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('[data-start]');
-const daysEl = document.querySelector('[data-days]');
-const hoursEl = document.querySelector('[data-hours]');
-const minutesEl = document.querySelector('[data-minutes]');
-const secondsEl = document.querySelector('[data-seconds]');
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
 let selectedDate = null;
 
-startBtn.setAttribute('disabled', '');
-startBtn.addEventListener('click', onStartBtnClick);
+disableStartBtn();
 
 const options = {
   enableTime: true,
@@ -20,54 +21,68 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
+    selectedDate = selectedDates[0].getTime();
+    const timeDifference = selectedDate - Date.now();
+
+    if (timeDifference <= 0) {
       Notify.failure('Please choose a date in the future');
-    } else {
-      startBtn.removeAttribute('disabled');
-      selectedDate = selectedDates[0];
+      return;
     }
+
+    Notify.success('You were choose the right date!');
+    enableStartBtn();
+    disableInput();
   },
 };
+const fp = flatpickr(refs.input, options); // flatpickr
 
-flatpickr(inputEl, options);
+refs.startBtn.addEventListener('click', onStartBtnClick);
 
 function onStartBtnClick() {
-  inputEl.setAttribute('disabled', '');
-  startBtn.setAttribute('disabled', '');
+  const intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const { days, hours, minutes, seconds } = convertMs(selectedDate - currentTime);
 
-  const timerId = setInterval(() => {
-    const timerDate = convertMs(selectedDate.getTime() - new Date().getTime());
-
-    daysEl.textContent = addLeadingZero(timerDate.days.toString());
-    hoursEl.textContent = addLeadingZero(timerDate.hours.toString());
-    minutesEl.textContent = addLeadingZero(timerDate.minutes.toString());
-    secondsEl.textContent = addLeadingZero(timerDate.seconds.toString());
-
-    if (
-      (daysEl.textContent === '00') &
-      (hoursEl.textContent === '00') &
-      (minutesEl.textContent === '00') &
-      (secondsEl.textContent === '00')
-    ) {
-      clearInterval(timerId);
+    if ((days === 0) & (hours === 0) & (minutes === 0) & (seconds === 0)) {
+      clearInterval(intervalId);
     }
+
+    refs.days.textContent = addLeadingZero(days.toString());
+    refs.hours.textContent = addLeadingZero(hours.toString());
+    refs.minutes.textContent = addLeadingZero(minutes.toString());
+    refs.seconds.textContent = addLeadingZero(seconds.toString());
   }, 1000);
+
+  disableStartBtn();
 }
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = Math.floor(ms / day); // Remaining days
+  const hours = Math.floor((ms % day) / hour); // Remaining hours
+  const minutes = Math.floor(((ms % day) % hour) / minute); // Remaining minutes
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second); // Remaining seconds
 
   return { days, hours, minutes, seconds };
 }
 
 function addLeadingZero(value) {
-  return value.padStart(2, '0');
+  return value.padStart(2, 0);
+}
+
+function disableStartBtn() {
+  refs.startBtn.disabled = true;
+}
+
+function enableStartBtn() {
+  refs.startBtn.disabled = false;
+}
+
+function disableInput() {
+  refs.input.disabled = true;
 }
